d61e0aed0487169bbbda2bb19e375bc17bc231e6
Tolq1n <tolq1n127608@gmail.com>
2024-03-19 06:08:04 +0500

    added swagger and created User APIs

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kszp(y6ljl6eogql8j#se_u*c9uh12gb90^%nfnphry0kn50qp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework.authtoken',
+    'rest_framework',
+    'drf_spectacular',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +131,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS':'drf_spectacular.openapi.AutoSchema',
+}
